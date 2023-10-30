e016cd40f2affbbd54da123764088103cc3e5e7a
Moe <moeali00000@gmail.com>
2023-10-30 18:04:50 +0200

    commity

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,17 +12,12 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *current = list;
-	void **points;
-	int size_buffer = 1024;
+	void *points[1024];
 	int count_points = 1;
 	int i;
 
 	if (!list || !list->next)
 		return (0);
-
-	points = malloc(8 * size_buffer);
-	if (!points)
-		return (-1);
 
 	points[count_points - 1] = list->next;
 	current = current->next;
@@ -32,16 +27,11 @@ int check_cycle(listint_t *list)
 		for (i = 0; i < count_points; i++)
 		{
 			if (current->next == points[i])
-			{
-				free(points);
 				return (1);
-			}
 		}
 		points[count_points] = current->next;
 		count_points++;
 		current = current->next;
 	}
-
-	free(points);
 	return (0);
 }
