e0df49ec666f7dfb9f4f2204b3090609eb85c03d
Moe <moeali00000@gmail.com>
2023-11-01 00:02:15 +0200

    commity

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 for number in range(97, 123):
     if number == 101 or number == 113:
         continue
-    print(f"{number:c}", end="")
+    print("{:c}".format(number), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for n1 in range(10):
     for n2 in range(n1 + 1, 10):
-        print(f"{n1:d}{n2:d}", end="\n" if n1 == 8 and n2 == 9 else ", ")
+        print("{:d}{:d}".format(n1, n2), end="\n" if n1 == 8 and n2 == 9 else ", ")
