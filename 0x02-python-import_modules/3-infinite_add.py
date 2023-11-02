76dbc4293bfd3f67d68edfe29ca5eb31e95c76b8
Moe <moeali00000@gmail.com>
2023-11-02 17:46:36 +0200

    commity

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv) - 1
+    lst = sys.argv
+    sum = 0
+
+    if n > 0:
+        for arg in lst:
+            if arg != sys.argv[0]:
+                sum += int(arg)
+    print ("{}".format(sum))
