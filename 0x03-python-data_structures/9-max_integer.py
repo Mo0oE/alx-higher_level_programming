7001bfceaeb5a7add53e08c4e60889dbc28510e0
Moe <moeali00000@gmail.com>
2023-11-06 21:56:02 +0200

    commity

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_list = my_list[0]
+
+    for i in my_list:
+        if i > max_list:
+            max_list = i
+    return max_list
